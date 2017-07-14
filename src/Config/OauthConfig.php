eabdd7d34fcdd417958ace094249343c5dfae716
git <git@222.84.159.240>
2017-07-14 17:15:57 +0800

    github

--- src/Config/OauthConfig.php
+++ src/Config/OauthConfig.php
@@ -15,6 +15,11 @@ return [
 			'app_id' => 'YOUR APP ID',
 			'app_secret' => 'YOUR APP SECRET',
 			'redirect_url' => ''
+		],
+		'github' => [
+			'app_id' => 'YOUR APP ID',
+			'app_secret' => 'YOUR APP SECRET',
+			'redirect_url' => ''
 		]
 	],
 
