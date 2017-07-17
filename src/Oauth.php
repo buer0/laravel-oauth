a8bff69f9417ff3adf95ce1ad0883889c8ef67d4
git <git@222.84.159.240>
2017-07-17 11:40:53 +0800

    all

--- src/Oauth.php
+++ src/Oauth.php
@@ -11,7 +11,7 @@ class Oauth
 	public static function routes($callback = null, array $options = [])
 	{
 		$callback = $callback ?: function($router) {
-			$router->registerRoute();
+			$router->all();
 		};
 
 		$defaultOptions = [
